8d6e16a7ac2565ede2d73af21b3b9d44e618b451
AlexKVal <alexkval@gmail.com>
2016-01-03 19:04:21 +0200

    [added] get 'model' from reddit json API

--- app/routes/r.js
+++ app/routes/r.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return Ember.$.getJSON(`https://www.reddit.com/r/${params.subreddit}.json`);
+  },
+
+  afterModel(r) {
+    r.subreddit = r.data.children[0].data.subreddit || '';
+  }
 });
