db15d7ad9c3ce74410d509ec16aaa0d0f8d0abad
AlexKVal <alexkval@gmail.com>
2016-01-03 19:36:34 +0200

    [added] get only imgur pictures

--- app/controllers/r.js
+++ app/controllers/r.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  pictures: Ember.computed('model.data.children.[]', function() {
+    let imgurOnly = this.get('model').data.children.filterBy('data.domain', 'i.imgur.com');
+    imgurOnly.forEach((pic) => {
+      if (pic.data.thumbnail === 'nsfw') {
+        pic.set('data.thumbnail', 'https://farm3.staticflickr.com/2571/3810679130_fbb7494d7b_t.jpg');
+      }
+    });
+
+    return imgurOnly;
+  })
 });
